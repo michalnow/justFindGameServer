569131dbdeabd3a36b303ca50ae42ec2d9d6ecc7
Michał Nowakowski <michal951@poczta.onet.pl>
2019-01-25 16:00:49 +0100

    added new fields and patterns to them

--- src/main/java/com/michal/justfindgame/eventCatalog/Event.java
+++ src/main/java/com/michal/justfindgame/eventCatalog/Event.java
@@ -2,6 +2,7 @@ package com.michal.justfindgame.eventCatalog;
 
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -11,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @Document
 @Data
@@ -26,7 +28,7 @@ public class Event {
     private int missing;
 
     @NotBlank(message = "must provide adress of event")
-    private String adress;
+    private String address;
 
     @NotBlank(message = "must provide difficulty of game")
     private String difficulty;
@@ -34,8 +36,15 @@ public class Event {
     @NotBlank(message = "must provide type of the event")
     private String type;
 
-    @NotBlank(message = "must provide when event takes place")
-    private String when;
+    @NotNull(message = "must provide date of the event")
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    private Date date;
+
+    @NotNull(message = "must provide time when the event starts")
+    @JsonFormat(pattern = "HH:mm")
+    private Date hour;
+
+    private String description;
 
 
 }
